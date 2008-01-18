2c8b80f37dfb5b6caf5539e725fc5311e59811fb
Taras Glek <tglek@mozilla.com>
2008-01-18 11:34:17 -0800

    testcase highlighting typedef mess

---
+++ test/typedef.cc
@@ -0,0 +1,13 @@
+typedef unsigned int PRUint32;
+typedef int PRInt32;
+typedef PRInt32 PRInt32_2;
+typedef PRInt32* PRInt322;
+
+class s {
+  typedef PRUint32 size_type;
+  size_type foo;
+  PRUint32 pru32;
+  PRInt322 i;
+};
+
+
