e907b712ad3bde865672af540322122babf6015f
Taras Glek <tglek@mozilla.com>
2008-03-25 15:43:47 -0700

    crap..left some test code in.

--- test/tree.js
+++ test/tree.js
@@ -1,9 +1,6 @@
 include ("treehydra.js")
 
 function process_tree(function_decl) {
-  print (function_decl.__iterator__)
-  print (function_decl.__iterator__)
-  return
   print (decl_name (function_decl) + "()")
   var b = DECL_SAVED_TREE (function_decl)
   sanity_check (b)
