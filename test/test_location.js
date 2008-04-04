0db9a85064adf5d6de6fb50901ec6edef317a335
David Mandelin <dmandelin@mozilla.com>
2008-04-04 11:37:51 -0700

    forgot to add earlier

---
+++ test/test_location.js
@@ -0,0 +1,31 @@
+include('treehydra.js');
+include('unit_test.js');
+//include('/home/dmandelin/treehydra-analysis/util.js')
+
+function process_tree(fndecl) {
+  let r = new TestResults();
+  new LocationTestCase(fndecl).run(r);
+  r.list();
+}
+
+function LocationTestCase(fndecl) {
+  this.fndecl = fndecl;
+}
+
+LocationTestCase.prototype = new TestCase();
+
+LocationTestCase.prototype.runTest = function() {
+  let body = fn_decl_body(this.fndecl);
+  for (let i = tsi_start (body); !i.end (i); i.next()) {
+    let stmt = i.stmt();
+    if (stmt.gstmt) {
+      let loc = stmt.gstmt.locus;
+      this.assertEquals(loc_is_valid(loc), true);
+    }
+  }
+}
+
+function loc_is_valid(loc) {
+  return loc != undefined && typeof loc.file == 'string' && 
+    typeof loc.line == 'number' && typeof loc.column == 'number';
+}
