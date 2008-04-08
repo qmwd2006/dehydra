6af4034c9d8ce273c27d4eec78212aed66232c83
David Mandelin <dmandelin@mozilla.com>
2008-04-07 19:19:11 -0700

    Bug 427092: fix up Treehydra/dehydra deps in app and tests

--- test/unit_test_harness.py
+++ test/unit_test_harness.py
@@ -4,9 +4,9 @@ from unittest import TestCase, TestSuite, TestLoader, TestResult
 
 class PluginTestCase(TestCase):
     """Test case for running Treehydra and checking output."""
-    def __init__(self):
+    def __init__(self, plugin):
         super(PluginTestCase, self).__init__()
-        self.plugin = "dehydra"
+        self.plugin = plugin
 
     def runTest(self):
         cmd = self.getCommand()
@@ -22,8 +22,8 @@ class CLITestCase(PluginTestCase):
     """Test case class for looking for expected items in the error output;
     or for no error output."""
 
-    def __init__(self, script_path, ccfile, *expected_errors):
-        super(CLITestCase, self).__init__()
+    def __init__(self, plugin, script_path, ccfile, *expected_errors):
+        super(CLITestCase, self).__init__(plugin)
         self.filename = script_path
         self.ccfile = ccfile
         self.expected_errors = expected_errors
@@ -39,17 +39,12 @@ class CLITestCase(PluginTestCase):
     def __str__(self):
        return "<test %s %s>"%(self.filename, self.ccfile)
 
-class TreehydraCLITestCase(CLITestCase):
-    def __init__(self, *args, **kw):
-        super(TreehydraCLITestCase, self).__init__(*args, **kw)
-        self.plugin = 'treehydra'
-
 class JSUnitTestCase(PluginTestCase):
     """Test case class for running a JS unit test and checking for OK
     in the results."""
 
-    def __init__(self, script_path, ccfile):
-        super(JSUnitTestCase, self).__init__()
+    def __init__(self, plugin, script_path, ccfile):
+        super(JSUnitTestCase, self).__init__(plugin)
         self.filename = script_path
         self.ccfile = ccfile
 
@@ -62,53 +57,82 @@ class JSUnitTestCase(PluginTestCase):
         last = lines[-1]
         self.failUnless(last.startswith('OK'), "Expected 'OK' output; got '%s'"%last)
 
-class TreehydraJSUnitTestCase(JSUnitTestCase):
-    def __init__(self, *args, **kw):
-        super(TreehydraJSUnitTestCase, self).__init__(*args, **kw)
-        self.plugin = 'treehydra'
-
 class MyTestResult(TestResult):
     """The default version stores formatted tracebacks for the failures.
     We want just the message."""
 
+    def addSuccess(self, *args, **kw):
+        super(MyTestResult, self).addSuccess(*args, **kw)
+        sys.stderr.write('.')
+
+    def addError(self, *args, **kw):
+        super(MyTestResult, self).addSuccess(*args, **kw)
+        sys.stderr.write('e')
+
     def addFailure(self, test, err):
         self.failures.append((test, err[1].args))
+        sys.stderr.write('x')
 
-if len(sys.argv) != 2:
-    print "usage: python %s <treehydra-cmd-fmt>"%(sys.argv[0])
+"""Assemble a test case from a test spec and the plugin to use."""
+def makeTest(plugin, spec):
+    cls = spec[0]
+    args = [ plugin ] + list(spec[1:])
+    return cls(*args)
+
+BOTH_TESTS = [
+    # These should all produce errors
+    (CLITestCase, 'nofile.js', 'empty.cc', 'Cannot find include file'),
+    (CLITestCase, 'syntax_error.js', 'empty.cc', 'SyntaxError'),
+    (CLITestCase, 'semantic_error.js', 'empty.cc', 'TypeError'),
+
+    # Ensure that the full path appears in error message
+    (CLITestCase, 'lib_error.js', 'empty.cc', 'TypeError', '/treehydra.js'),
+
+    # Test 'include'
+    (JSUnitTestCase, 'test_include.js', 'empty.cc'),
+    (JSUnitTestCase, 'test_include2.js', 'empty.cc'),
+
+    # Test 'require'
+    (JSUnitTestCase, 'test_require.js', 'empty.cc')
+]
+
+DEHYDRA_TESTS = [
+    # This will produce a JS exception, but should not produce linker errors
+    (CLITestCase, 'tc_pass1.js', 'onefunc.cc', 'Unrecognized require keyword'),
+]
+
+TREEHYDRA_TESTS = [
+    # Require pass option tests
+    (CLITestCase, 'tc_pass1.js', 'onefunc.cc'),
+    (CLITestCase, 'tc_pass2.js', 'onefunc.cc', 'Cannot set gcc_pass_after'),
+    # Source location conversion
+    (JSUnitTestCase, 'test_location.js', 'onefunc.cc'),
+]
+    
+if len(sys.argv) != 3:
+    print "usage: python %s <test-set> <*hydra-cmd-fmt>"%(sys.argv[0])
     sys.exit(1)
 
-# This should be a format string with 2 %s, the first for the script,
-# the second for the C++ file.
-TREEHYDRA_CMD_FORMAT = sys.argv[1]
+# This should be a format string with 3 %s, the plugin, script, and C++ file
+TEST_SET = sys.argv[1]
+TREEHYDRA_CMD_FORMAT = sys.argv[2]
+
+if TEST_SET == 'dehydra':
+    tests = [ makeTest('dehydra', t) for t in BOTH_TESTS + DEHYDRA_TESTS ]
+elif TEST_SET == 'treehydra':
+    tests = [ makeTest('treehydra', t) for t in BOTH_TESTS + TREEHYDRA_TESTS ]
+elif TEST_SET == 'both':
+    tests = [ makeTest('dehydra', t) for t in BOTH_TESTS + DEHYDRA_TESTS ] + \
+        [ makeTest('treehydra', t) for t in BOTH_TESTS + TREEHYDRA_TESTS ]
+else:
+    print "%s: <test-set> must be one of dehydra, treehydra, both"%(sys.argv[0])
 
 s = TestSuite()
-
-# Ensure that all of these produce errors.
-s.addTest(CLITestCase('nofile.js', 'empty.cc', 'Cannot find include file'))
-s.addTest(CLITestCase('syntax_error.js', 'empty.cc', 'SyntaxError'))
-s.addTest(CLITestCase('semantic_error.js', 'empty.cc', 'TypeError'))
-
-# Ensure that the full path appears
-s.addTest(CLITestCase('lib_error.js', 'empty.cc', 'TypeError', '/treehydra.js'))
-
-# Require pass tests
-s.addTest(TreehydraCLITestCase('tc_pass1.js', 'onefunc.cc'))
-s.addTest(TreehydraCLITestCase('tc_pass2.js', 'onefunc.cc', 'Cannot set gcc_pass_after'))
-
-# Run Dehydra unit tests
-s.addTest(JSUnitTestCase('test_include.js', 'empty.cc'))
-s.addTest(JSUnitTestCase('test_include2.js', 'empty.cc'))
-s.addTest(JSUnitTestCase('test_require.js', 'empty.cc'))
-s.addTest(JSUnitTestCase('test_skipthis.js', 'skipthis.cc'))
-
-# Treehydra unit tests
-s.addTest(TreehydraJSUnitTestCase('test_location.js', 'onefunc.cc'))
-
-
+for t in tests: s.addTest(t)
 r = MyTestResult()
 
 s.run(r)
+sys.stderr.write('\n')
 
 if r.wasSuccessful():
     print "OK: %d tests passed"%r.testsRun
