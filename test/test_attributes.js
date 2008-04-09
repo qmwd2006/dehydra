3261aaa43a0f74e836bf204293ccd5a83a3ad583
Taras Glek <tglek@mozilla.com>
2008-04-08 18:16:46 -0700

    Forgot to commit the actual testcase

---
+++ test/test_attributes.js
@@ -0,0 +1,17 @@
+function process_type(c) {
+  //print (c)
+}
+
+function process_function(f,arr) {
+  //print(arr)
+}
+
+function process_var(v) {
+  if (v.name == "varAttrOnType") {
+    var a = v.type.attributes[0]
+    if (a.name != "user" || a.value[0] != "value")
+      throw new Error ("Attributes on types don't work")
+    print("OK")
+  }
+}
+
