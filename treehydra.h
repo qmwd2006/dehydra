404346063808191e1607760331790193fba609d1
Taras Glek <tglek@mozilla.com>
2008-04-14 16:21:12 -0700

    bug 427661: Make primitives more like dehydra_defineproperty such that they don't need to do any rooting

--- treehydra.h
+++ treehydra.h
@@ -11,10 +11,6 @@ jsval get_lazy (struct Dehydra *this, treehydra_handler handler, void *v,
                struct JSObject *parent, const char *propname);
 jsval get_existing_or_lazy (struct Dehydra *this, treehydra_handler handler, void *v,
                             struct JSObject *parent, const char *propname);
-jsval convert_int (struct Dehydra *this, int i);
-jsval convert_location_t (struct Dehydra *this, location_t loc);
- 
-jsval convert_char_star (struct Dehydra *this, const char *str);
 jsval get_enum_value (struct Dehydra *this, const char *name);
 
 void lazy_tree_node (struct Dehydra *this, void *structure, struct JSObject *obj);
@@ -26,4 +22,9 @@ struct JSObject *dehydra_defineArrayProperty (struct Dehydra *this,
 
 void convert_tree_node_union (struct Dehydra *this, enum tree_node_structure_enum code,
                               union tree_node *var, struct JSObject *obj);
+void convert_int (struct Dehydra *this, struct JSObject *parent, const char *propname, int i);
+void convert_location_t (struct Dehydra *this, struct JSObject *parent,
+                         const char *propname, location_t loc);
+void convert_char_star (struct Dehydra *this, struct JSObject *parent,
+                        const char *propname, const char *str);
 #endif
