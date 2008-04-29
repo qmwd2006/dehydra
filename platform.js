88fb63392ca1eae5007fb3f87d626b70ea0e2523
Taras Glek <tglek@mozilla.com>
2008-04-29 15:17:05 -0700

    bug 431236:Make sure that inner functions aren't called lazily

--- platform.js
+++ platform.js
@@ -1,1 +1,8 @@
 const isGCCApple = (sys.gcc_version.indexOf('Apple') != -1);
+
+//Note sure if this is the best place to keep this
+function EnumValue (name, value) {
+  this.name = name
+  this.value = value
+}
+
