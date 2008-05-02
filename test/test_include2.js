476cddc2bef6d46da283a00ed7b8aecf834221cf
Taras Glek <tglek@mozilla.com>
2008-05-02 14:56:21 -0700

    renamed system.js to dehydra.js

--- test/test_include2.js
+++ test/test_include2.js
@@ -16,10 +16,6 @@ include ("test_include2_lib.js" )
 if (includeCounter !== 1)
   throw new Error("Failed to do include")
 
-if (includeCounter !== 1)
-  throw new Error("Double inclusion shouldn't be possible")
-
 include("test_include2.js")
-include("system.js")
 print("OK")
 
