06343d6ba4f2573f8247be4b92235654966f1b2b
Taras Glek <tglek@mozilla.com>
2008-07-03 12:01:55 -0700

    bug 443411: isStatic does not reflect same thing as it means in sourcecode

---
+++ test/test_static_func.js
@@ -0,0 +1,22 @@
+// { 'test': 'dehydra', 'input': 'test_static_func.cc', 'output': 'unit_test' }
+function assert (t) {
+  if (!t)
+    throw new Error("static isn't correct");
+}
+
+function process_function(f) {
+  print(f.name + f.isStatic)  
+  switch (f.name) {
+  case "X::st()":
+  case "gst()":    
+  case "<unnamed>::nst()":
+    assert(f.isStatic)
+    break;
+  default:
+    assert(!f.isStatic)
+  }
+}
+
+function input_end() {
+  print("OK");
+}
