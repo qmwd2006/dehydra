52640e57c351915428dee9bb7b25b29db4b2cbe5
Taras Glek <tglek@mozilla.com>
2009-05-11 11:35:06 -0700

    Bug 492090: Make dehydra less angry r=zwol

--- xassert.h
+++ xassert.h
@@ -4,7 +4,11 @@
 
 #define xassert(cond)                                                   \
   if (!(cond)) {                                                        \
-    fprintf(stderr, "%s:%d: Assertion failed:" #cond "\n",  __FILE__, __LINE__); \
+    fprintf(stderr, "%s:%d: Assertion failed:" #cond                    \
+            ". \nIf the file compiles correctly without invoking "      \
+            "dehydra please file a bug, include a testcase or .ii file" \
+            " produced with -save-temps\n",                             \
+            __FILE__, __LINE__);                                        \
     _exit(1);                                                           \
   }
 
