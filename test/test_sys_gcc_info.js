f92c70ee276b64a87966db5ef2e2dc196c8d5f69
Taras Glek <tglek@mozilla.com>
2009-05-27 14:39:21 -0700

    clarifying comment

--- test/test_sys_gcc_info.js
+++ test/test_sys_gcc_info.js
@@ -8,8 +8,8 @@ function SysTest(v1, v2) {
 SysTest.prototype = new TestCase();
 SysTest.prototype.runTest = function()
 {
+  // aux_base_name has the filename passed to gcc -extension
   this.assertEquals(sys.aux_base_name, "empty");
-  this.assertTrue(/\d+\.\d+\.\d+/(sys.gcc_version))
 }
 
 var r = new TestResults()
