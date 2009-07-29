9a3d8fffbdf250a5e77749b11cba117618372038
Dan Witte <dwitte@mozilla.com>
2009-07-28 17:48:31 -0700

    Typo fix.

--- test/test_treehydra_decl_as_string.js
+++ test/test_treehydra_decl_as_string.js
@@ -1,6 +1,6 @@
 // { 'test': 'treehydra', 'input': 'assign.cc', 'output': 'unit_test' }
 
 function process_tree(d) {
-  if (d._decl_name.str.indexOf("foo()") != -1)
+  if (d._decl_as_string.str.indexOf("foo()") != -1)
     print("OK")
 }
