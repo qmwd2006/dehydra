57457ec6c9d794c54e522c3b6d0ebf89484991c1
Ehren Metcalfe <ehren.m@gmail.com>
2010-11-22 05:38:45 -0500

    Bug 589240 - C++ testcase for resolve_virtual_fn_addr_exprs

---
+++ test/virtual_fn_addr_exprs.cc
@@ -0,0 +1,59 @@
+class Base {
+public:
+  virtual void b();
+  virtual void a();
+};
+
+class Derived : public Base {
+  double z;
+ public:
+  virtual void a();
+  int e();
+  int x;
+  int y;
+  virtual void b();
+  virtual void c();
+  virtual double d(int x);
+};
+
+class NotDerived {
+ public:
+  virtual void a(int x);
+  virtual void b(double y);
+  virtual int c();
+};
+
+typedef typeof(&Base::a) aPtrBase;
+typedef typeof(&Base::b) bPtrBase;
+typedef typeof(&Derived::a) aPtrDerived;
+
+struct A
+{
+  aPtrBase ptr;
+};
+
+struct B {
+  A array[2];
+};
+
+A a_arr_glob[] = { { reinterpret_cast<aPtrBase>(&Derived::a) }, { &Base::a } };
+static B b_arr_glob[] = { { { &Base::a, reinterpret_cast<aPtrBase>(&Derived::a) } } };
+
+aPtrBase a_ptr_glob = &Base::a;
+aPtrBase b_ptr_glob = reinterpret_cast<aPtrBase>(&NotDerived::b);
+
+void bar(aPtrBase);
+
+void foo() {
+  static A a_arr[] = { { reinterpret_cast<aPtrBase>(&Derived::a) }, { &Base::a } };
+  A a_arr2[] = { { reinterpret_cast<aPtrBase>(&Derived::a) }, { &Base::a } };
+  aPtrBase aptr = reinterpret_cast<aPtrBase>(&Derived::a);
+  aPtrBase really_b_ptr = reinterpret_cast<aPtrBase>(&NotDerived::b);
+  static B b_arr[] = { { { &Base::a, reinterpret_cast<aPtrBase>(&Derived::a) } } };
+  B b_arr2[] = { { { reinterpret_cast<aPtrBase>(&Derived::a), &Base::a } } };
+  static B b_arr3[] = { { { reinterpret_cast<aPtrBase>(&Derived::d), reinterpret_cast<aPtrBase>(&Derived::c) } } };
+  aPtrDerived derived = reinterpret_cast<aPtrBase>(&NotDerived::b);
+
+  // In GENERIC but not GIMPLE, this call arg is of type PTRMEM_CST.
+  bar(reinterpret_cast<aPtrBase>(&NotDerived::c));
+}
